4101636a3a0cc1553c5ad2296385166d3106da53
Кирилл Завадский <zavad93@mail.ru>
2025-05-08 13:05:55 +0300

    update setup_progress

--- view/setup_progress.py
+++ view/setup_progress.py
@@ -21,9 +21,25 @@ async def setup_progress(
         progress.target_value = value_data.target_value
         progress.current_value = value_data.current_value
         progress.value = (
-            (value_data.start_value - value_data.current_value)
-            / (value_data.start_value - value_data.target_value) * 100
+            abs(value_data.start_value - value_data.current_value)
+            / abs(value_data.start_value - value_data.target_value) * 100
         )
+        if progress.change_body_program_id == 1 and value_data.start_value < value_data.target_value:
+            raise HTTPException(
+                status_code=400,
+                detail=(
+                    "Выбранная программа - Похудение."
+                    "Стартовый вес должен быть выше цели."
+                )
+            )
+        if progress.change_body_program_id == 2 and value_data.start_value > value_data.target_value:
+            raise HTTPException(
+                status_code=400,
+                detail=(
+                    "Выбранная программа - Набор массы."
+                    "Стартовый вес должен быть ниже цели."
+                )
+            )
         db.add(progress)
         await db.commit()
         return {
